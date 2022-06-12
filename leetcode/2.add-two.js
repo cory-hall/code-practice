f84755010e48b3dbbdbded41784fceea86933e69
Cory Hall <ComebackCory1@gmail.com>
2022-06-12 13:02:47 -0500

    need to finish 2.

---
+++ leetcode/2.add-two.js
@@ -0,0 +1,35 @@
+const addTwoNumbers = function (l1, l2) {
+  var List = new ListNode(0);
+  var head = List;
+  var sum = 0;
+  var carry = 0;
+
+  while ((l1 || l2)) {
+    if (l1 !== null) {
+      sum = sum + l1.val;
+      l1 = l1.next;
+    }
+    if (l2 !== null) {
+      sum = sum + l2.val;
+      l2 = l2.next;
+    }
+    if (sum >= 10) {
+      carry = 1;
+      sum = sum - 10;
+    }
+
+    head.next = new ListNode(sum);
+    head = head.next;
+
+    sum = carry;
+    carry = 0;
+  }
+  return List.next;
+};
+
+function ListNode(val, next) {
+  this.val = (val === undefined ? 0 : val)
+  this.next = (next === undefined ? null : next)
+}
+
+module.exports = { addTwoNumbers, ListNode };
